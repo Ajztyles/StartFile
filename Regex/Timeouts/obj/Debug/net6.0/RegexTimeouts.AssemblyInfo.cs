92b5611ababd12c7ddc3dd6ae1d78657450702e3
Ajztyles <Ajsolomon62@gmail.com>
2025-02-20 10:28:22 +0100

    RegexTimeouts

--- Regex/Timeouts/obj/Debug/net6.0/RegexTimeouts.AssemblyInfo.cs
+++ Regex/Timeouts/obj/Debug/net6.0/RegexTimeouts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RegexTimeouts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec956b7979d62847c080e3f13370d7360a10ec4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddc79d3d589f0bf148a14541b5e1b2bb67f6e3d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RegexTimeouts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RegexTimeouts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
